ff6c43729f004a5a231254947c9922cde467c21a
TomArcher <tarcher@microsoft.com>
2016-04-02 13:07:19 -0700

    implemented issue04

--- code/AcomCmdlets/Cmdlets/Get-AcomArticle.cs
+++ code/AcomCmdlets/Cmdlets/Get-AcomArticle.cs
@@ -13,16 +13,8 @@ namespace Microsoft.AcomCmdlets.Commands
     [Cmdlet(VerbsCommon.Get, "AcomArticle", DefaultParameterSetName = "Author")]
     public class GetAcomArticleCommand : Cmdlet
     {
-        /// <summary>
-        /// Specifies the ACOM article file name the user wants to process.
-        /// </summary>
-        [Parameter(
-            Mandatory = true,
-            Position = 0,
-            ValueFromPipeline = true
-        )]
-        [ValidateNotNullOrEmpty]
-        public string FileName { get; set; }
+        [Parameter(Mandatory = true, Position =0)]
+        public string ArticleDirectory { get; set; }
 
         [Parameter(ParameterSetName ="Author", Mandatory = false)]
         [Parameter(ParameterSetName = "Stale", Mandatory = false)]
@@ -34,41 +26,54 @@ namespace Microsoft.AcomCmdlets.Commands
 
         [Parameter(ParameterSetName = "StaleWithin", Mandatory = true)]
         [ValidateRange(1, 89)]
+        [PSDefaultValue()]
         public int StaleWithin { get; set; } = 0;
 
         const int staleDays = 90;
 
         protected override void ProcessRecord()
         {
-            if (this.FileName != null)
+            try
             {
-                try
+                foreach (string fileName in Directory.GetFiles(this.ArticleDirectory, "*.md", SearchOption.AllDirectories))
                 {
-                    AcomArticle article = new AcomArticle(this.FileName);
-
-                    // Check author
-                    if (this.Author != string.Empty && !article.Author.Contains(this.Author))
-                    {
-                        return; // Failed author check; no need to go further.
-                    }
-
-                    // Check staleness
-                    if (this.Stale || this.StaleWithin > 0)
-                    {
-                        if (article.PubDate.AddDays(staleDays - this.StaleWithin) > DateTime.Now)
-                        {
-                            return; // Article is fresh
-                        }
-                    }
-
-                    WriteObject(article);
+                     ProcessFile(fileName);
                 }
-                catch(Exception ex) 
+            }
+            catch (Exception ex)
+            {
+                ThrowTerminatingError(new ErrorRecord(ex, AcomCmdlets.Resources.UnexpectedError, ErrorCategory.InvalidOperation, this));
+            }
+        }
+
+        protected void ProcessFile(string fileName)
+        {
+            try
+            {
+                AcomArticle article = new AcomArticle(fileName);
+
+                // Check author
+                if (this.Author != string.Empty && !article.Author.Contains(this.Author))
                 {
-                    StringBuilder error = new StringBuilder();
-                    error.AppendFormat("[{0}] : {1}", this.FileName, ex.Message);
-                    ThrowTerminatingError(new ErrorRecord(new Exception(error.ToString(), ex), AcomCmdlets.Resources.UnexpectedError, ErrorCategory.InvalidOperation, this));
+                    return; // Failed author check; no need to go further with this article.
                 }
+
+                // Check staleness
+                if (this.Stale || this.StaleWithin > 0)
+                {
+                    if (article.PubDate.AddDays(staleDays - this.StaleWithin) > DateTime.Now)
+                    {
+                        return; // User asked for stale/stalewithin and this article doesn't match criteria.
+                    }
+                }
+
+                WriteObject(article);
+            }
+            catch(Exception ex)
+            {
+                StringBuilder error = new StringBuilder();
+                error.AppendFormat("[{0}] : {1}", fileName, ex.Message);
+                ThrowTerminatingError(new ErrorRecord(new Exception(error.ToString(), ex), AcomCmdlets.Resources.UnexpectedError, ErrorCategory.InvalidOperation, this));
             }
         }
     }
